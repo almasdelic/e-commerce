c334a2c2b650519a53c30f131526b65e2542134c
Almas <130715650+almasdelic@users.noreply.github.com>
2023-12-14 20:14:54 +0100

    Update cart.py
    Add update_cart

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -46,4 +46,20 @@ class Cart():
     def get_quants(self):
         quantities = self.cart
         return quantities
+    
+    
+    def update(self, product, quantity):
+        product_id = str(product)
+        product_qty = int(quantity)
+        # id is string and qty is integer because of the cart is dictionary, like this {'4':3, '2':5}
+        
+        # Get cart
+        ourcart = self.cart
+        # Update dictionary
+        ourcart[product_id] = product_qty
+        
+        self.session.modified = True
+        
+         
+        
         

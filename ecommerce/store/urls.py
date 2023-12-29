b73f59956335de98533753498b7c249786ae9192
Almas <130715650+almasdelic@users.noreply.github.com>
2023-12-29 21:41:27 +0100

    Update urls.py

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('thank_you/', views.thank_you, name='thank_you'),
     path('about/', views.about, name='about'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),

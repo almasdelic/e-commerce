1e5e52d9afc451aa9fc220a83336a8aefc1de2c7
Almas <130715650+almasdelic@users.noreply.github.com>
2023-12-29 21:17:11 +0100

    Update urls.py

--- ecommerce/cart/urls.py
+++ ecommerce/cart/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('add/', views.cart_add, name='cart_add'),
     path('delete/', views.cart_delete, name='cart_delete'),
     path('update/', views.cart_update, name='cart_update'),
+    path('checkout/', views.procced_to_checkout, name='checkout'),
 ]
  

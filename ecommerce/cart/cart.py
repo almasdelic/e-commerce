1cf170b7714f8e59861262f7d2163dd9aab0aa46
Almas <130715650+almasdelic@users.noreply.github.com>
2023-12-29 17:46:35 +0100

    Update cart.py
    Adding cart.total

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -68,6 +68,18 @@ class Cart():
             
         self.session.modified = True
         
-         
+    
+    def cart_total(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        quantities = self.cart
         
+        total = 0
+        for key, value in quantities.items():
+            key = int(key)
+            for product in products:
+                if product.id == key:
+                    total = total + (product.price * value)
+        
+        return total
         
